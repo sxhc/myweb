3121542fd4d9083d93cc5d46f4951f3141c27184
sxhc <243394130@qq.com>
2017-05-26 02:30:16 +0800

    admin

--- mainsite/models.py
+++ mainsite/models.py
@@ -13,6 +13,6 @@ class Post(models.Model):
         # ordering by pub_date
         ordering = ('-pub_date',)
     
-    def __unicode__(self):
+    def _unicode_(self):
         # show title 
         return self.title
