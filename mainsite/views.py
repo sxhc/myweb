695fe6a6b672bf88163ed03dc11587d61368f2d4
sxhc <243394130@qq.com>
2017-05-26 05:53:14 +0800

    use templates i

--- mainsite/views.py
+++ mainsite/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
+from django.template.loader import get_template
+from datetime import datetime
 
 # Create your views here.
 
 def homepage(request):
+    template = get_template('index.html')
     posts = Post.objects.all()
-    post_lists = list()
-    for count, post in enumerate(posts):
-        post_lists.append("No.{}:".format(str(count)) + str(post) +"<br>")
-    return HttpResponse(post_lists)
+    now = datetime.now()    
+    html = template.render(locals())
+    return HttpResponse(html)
