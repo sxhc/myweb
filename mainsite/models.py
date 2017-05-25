6d95b9257b26bf609f808a3a9149fcfdbe09eaef
sxhc <243394130@qq.com>
2017-05-26 02:09:31 +0800

    creat Post Model

--- mainsite/models.py
+++ mainsite/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+#creat models
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    webadd = models.CharField(max_length=200)
+    body = models.TextField()
+    pub_date = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        # ordering by pub_date
+        ordering = ('-pub_date',)
+    
+    def __unicode__(self):
+        # show title 
+        return self.title
